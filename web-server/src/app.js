77a87493e8596d902d60b4bfc91bccf469a6a299
yschristian <yubahwesc@gmail.com>
2022-01-23 06:22:43 -0800

    edited

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -6,37 +6,37 @@ const app=express();
 //define path for express config
 const pat=path.join(__dirname,'../public');
 const viewPath = path.join(__dirname,'../templates/views');
-const partialsPath = path.join(__dirname,'..templates/partials');
+const partialsPath = path.join(__dirname,'../templates/partials');
 
 //set up handlebars engine and view location
 app.set('view engine','hbs');
 app.set('views',viewPath);
-hbs.registerPartial(partialsPath);
+hbs.registerPartials(partialsPath);
 
 //sending static  directory  to  server
 app.use(express.static(pat))
 
-app.get('',(req,res)=>{
+app.get("",(req,res)=>{
     res.render('index',{
        title:'Weather',
-        name:'chris rex'
+        name:'Yubahwe Chris'
 
     });
 })
 app.get('/about',(req,res)=>{
 
     res.render('about',{
-        title:'about me,',
-        name:'chris  rex'
-    })
+        title:'about',
+        name:'Yubahwe Chris'
+    });
 })
 app.get('/help',(req,res)=>{
 
     res.render('help',{
-        title:'any help,',
-        name:'chris  rex'
-          
-    })
+        title:'any help',
+        name:'Yubahwe Chris',
+        helpText: 'This is some helpful text.'
+    });
 })
 
 app.get('/weather',(req,res)=>{
@@ -46,19 +46,19 @@ app.get('/weather',(req,res)=>{
           
     })
 })
-app.get('help/*',(req,res)=>{
+app.get('/help/*',(req,res)=>{
     res.render('404',{
         title:'404',
-        name:'chris rex',
+        name:'Yubahwe Chris',
         errorMessage:"help article not found"
     })
 })
 app.get('*',(req,res)=>{
 res.render('404',{
     title:'404',
-    name:'chris rex',
+    name:'Yubahwe Chris',
     errorMessage:"page not found"
-})
+  })
 })
 app.listen(3000,()=>{
     
